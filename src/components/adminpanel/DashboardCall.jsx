fdb0255a6a27a4ca557b25d358121def38aea761
Jertroll <jeremyblanco20@gmail.com>
2024-10-27 01:51:05 -0600

    Panel admin

--- src/components/adminpanel/DashboardCall.jsx
+++ src/components/adminpanel/DashboardCall.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import Dashboard from './Dashboard'
+// DashboardCall.js
+import Navbar from "./Navbar";
+import Sidebar from './Sidebar';
+
+import { useState } from 'react';
+
 function DashboardCall() {
+  const [activeTab, setActiveTab] = useState(''); // Estado para el tab activo
+
   return (
-    <div className='flex'>
-        <Sidebar/>
-        <Dashboard/>
+    <div className="flex h-screen">
+      <Sidebar setActiveTab={setActiveTab} />
+      
+      <div className="flex-1">
+        <Navbar />
+        <div className="w-full p-7">
+          {/*          {activeTab === 'usuarios' && <UserCrud />}
+          {activeTab === 'productos' && <ProductsCrud />}
+          {activeTab === 'informes' && <Reports />} */}
+
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DashboardCall
+export default DashboardCall;
--- src/components/adminpanel/Navbar.jsx
+++ src/components/adminpanel/Navbar.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
-import { FaBars } from 'react-icons/fa'
+import { FaBell, FaSearch, FaUserCircle } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <div className='bg-grey-800 px-4 py-3 flex justify-between'>
-      <div className='flex item-center text-xl'>
-        <FaBars className='text-white me-4 cursor-pointer'/>
-        <span>E-comerce</span>
+    <nav className="bg-gray-800 px-4 py-3 flex justify-between items-center">
+      <div className="flex items-center gap-x-5">
+        {/* Input de búsqueda al inicio */}
+        <div className="relative w-64 hidden md:block">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+            <button className="p-1 focus:outline-none text-white md:text-black">
+              <FaSearch />
+            </button>
+          </span>
+          <input
+            type="text"
+            placeholder="Buscar..."
+            className="w-full px-4 py-1 pl-10 rounded shadow outline-none"
+          />
+        </div>
       </div>
-      <div></div>
-    </div>
-  )
+
+      <div className="flex items-center gap-x-5">
+        <div className="text-white">
+          <FaBell className="w-6 h-6" />
+        </div>
+
+        <div className="relative">
+          <button className="text-white group">
+            <FaUserCircle className="w-6 h-6 mt-1" />
+          </button>
+          <div className="hidden absolute bg-white rounded-lg shadow w-32 group-focus:block top-full right-0">
+            <ul className="py-2 text-sm text-gray-900">
+              <li>
+                <a href="#">Perfil</a>
+              </li>
+              <li>
+                <a href="#">Configuración</a>
+              </li>
+              <li>
+                <a href="#">Cerrar Sesión</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
