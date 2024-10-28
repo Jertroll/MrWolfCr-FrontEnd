6d75338ea9f84ed8e6e339f0c46081493304d052
Jertroll <jeremyblanco20@gmail.com>
2024-10-28 01:45:18 -0600

    Registro usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import DashboardCall from './components/adminpanel/DashboardCall'
+import RegistroUsuario from './components/usuario/RegistroUsuario'
+import RegistroUsuarioCliente from './components/usuario/RegistroUsuarioCliente'
 
 function App() {
   return (
     <>
-      <DashboardCall/>
+      <RegistroUsuarioCliente/>
+
     </>
   )
 }
