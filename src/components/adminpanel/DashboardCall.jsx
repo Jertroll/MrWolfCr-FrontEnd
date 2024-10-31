c02244c9b93d2d18f8a10551095d0ab34065dbfe
Kimyu15 <kimberley.gomez.quesada@est.una.ac.cr>
2024-10-31 10:44:23 -0600

    rutas

--- src/components/adminpanel/DashboardCall.jsx
+++ src/components/adminpanel/DashboardCall.jsx
@@ -1,8 +1,10 @@
 // DashboardCall.js
+import { Outlet, BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from './Sidebar';
 
 import { useState } from 'react';
+import TableReact from "../tableReact/TableReact";
 
 
 
@@ -10,14 +12,26 @@ function DashboardCall() {
   const [activeTab, setActiveTab] = useState(''); // Estado para el tab activo
 
   return (
-    <div className="flex h-screen">
-      <Sidebar setActiveTab={setActiveTab} />
-      
-      <div className="flex-1">
-        <Navbar />
+    <BrowserRouter>
+      <div className="flex h-screen">
+        <Sidebar setActiveTab={setActiveTab} />
 
+        <div className="flex-1">
+          <Navbar />
+          <div className="content" >
+            <Routes>
+
+              <Route  path="/" element={<></>}/>
+              <Route  path="/usuario" element={<TableReact/>}/>
+              <Route  path="/estadistica" element={<h1>Holamundo</h1>}/>
+              <Route  path="/config" element={<TableReact/>}/>
+            </Routes>
+          </div>
+        </div>
       </div>
-    </div>
+
+    </BrowserRouter>
+
   );
 }
 
