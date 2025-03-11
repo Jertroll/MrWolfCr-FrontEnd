4ed7701b8587ea524d4070203b16c6efb391c452
Jertroll <jeremyblanco20@gmail.com>
2025-03-11 03:20:59 -0600

    css home

--- src/components/VistaCliente/Paginas/Home.jsx
+++ src/components/VistaCliente/Paginas/Home.jsx
@@ -1,11 +1,14 @@
 import NavbarCliente from "../navbar/NavbarCliente";
+import Footer from "../footer/FooterCliente";
+import "./Home.css";
 
 function Home() {
   return (
-    <>
+    <div className="home-container">
       <NavbarCliente />
-      <div>Home</div>
-    </>
+      <div className="content">Home</div>
+      <Footer />
+    </div>
   );
 }
 
